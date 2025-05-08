a352d1e41a4765eefad584cc13a2ec16d5ffb058
Vikash-sain1507 <sainvikash1507@gmail.com>
2025-05-08 18:29:14 +0530

    add more files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
 import './App.css';
+import Carousel from './component/Carousel';
+import Cours from './component/Cours';
+import Footer from './component/Footer';
+import Header from './component/Header';
+import Sidebar from './component/Sidebar';
+import Home from './component/Home'; 
+import Hindi from './component/Hindi';
+import LayOut from './component/LayOut';
 
 function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header />} />
+      </Routes>
+      <Routes>
+        <Route path="/" element={<Carousel />} />
+      </Routes>
+      <Routes>
+        <Route path='/' element={<Cours />} />
+      </Routes>
+      <Routes>
+        <Route path='/' element={<LayOut />} />
+      </Routes>
+      <Routes>
+        <Route path="/" element={<Footer />} />
+      </Routes>
+      {/* Sidebar should be inside BrowserRouter */}
+      <Sidebar open={sidebarOpen} toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
+
+      {/* Correct Routes Setup */}
+      <Routes>
+        <Route path="/home" element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route path="/hindi" element={<Hindi />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
